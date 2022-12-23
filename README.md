5a19dddedcc549669eebc4c4731522eb209e6d72
Mark <diamindstudios@gmail.com>
2022-12-23 13:31:32 -0700

    create base controller & add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Template Node API (Express)
+
+This is a quick starting point for your Node APIs you develop.
+
+## Getting Started
+
+Simply clone the repository as a starting point and initialize a new .git project.
--- src/api/test/controller.ts
+++ src/api/test/controller.ts
@@ -1,41 +1,12 @@
 import express from 'express';
-import { ApiResponse } from '../types';
+import { Controller } from '../base.endpoint/base.controller';
 import { Test } from './types';
+// entity type = Test
 
-const path = '/test';
+export class TestController extends Controller {
+  public path = '/test';
 
-const buildResponse = (data: any, success: boolean): ApiResponse<any> => {
-  return {
-    success,
-    data: data,
-  };
-};
-
-export class TestController {
-  public router = express.Router();
-
-  private tests: Test[] = [{ message: 'this is a test message', id: '123abcHexId' }];
-
-  constructor() {
-    this.intializeRoutes();
-  }
-
-  intializeRoutes() {
-    this.router.get(path, this.getAllTests);
-    this.router.post(path, this.createTest);
-  }
-
-  getAllTests = (request: express.Request, response: express.Response): ApiResponse<Test[]> => {
-    const responseBody = buildResponse(this.tests, true);
-    response.send(responseBody);
-    return responseBody;
-  };
-
-  createTest = (request: express.Request, response: express.Response): ApiResponse<Test> => {
-    const test: Test = request.body;
-    this.tests.push(test);
-    const responseBody = buildResponse(test, true);
-    response.send(responseBody);
-    return responseBody;
+  getAll = (request: express.Request, response: express.Response) /*: ApiResponse<Test[]>*/ => {
+    super.getAll(request, response); // override call and perform additional logic
   };
 }
